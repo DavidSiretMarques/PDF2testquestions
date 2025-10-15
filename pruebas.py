1dd85c1c4dbb064817c3bd071cb43536d20d83ed
DavidSiretMarques <wowdmg97@gmail.com>
2025-10-15 16:30:09 +0200

    added pruebas.py to test stuff before adding to main program

---
+++ pruebas.py
@@ -0,0 +1,13 @@
+from pypdf import PdfReader
+
+reader = PdfReader("example_test2.pdf")
+text = ""
+for page in reader.pages:
+    text += page.extract_text() + "\n"
+    print(text)
+
+print(text)
+
+with open("text.txt", "w", encoding='utf-8') as f:
+    f.write(text)
+    
