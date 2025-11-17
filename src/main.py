824f0060f40dfc9219f8a2065d1e6dc7d7439cd5
DavidSiretMarques <wowdmg97@gmail.com>
2025-11-17 15:20:21 +0100

    changed to flet for the GUI, seems easier (might regret it...)

---
+++ src/main.py
@@ -0,0 +1,117 @@
+import flet as ft
+
+class TodoApp(ft.Column):
+    # application's root control is a Column containing all other controls
+    def __init__(self):
+        super().__init__()
+        self.new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+        self.tasks_view = ft.Column()
+        self.width = 600
+        self.controls = [
+            ft.Row(
+                controls=[
+                    self.new_task,
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD, on_click=self.add_clicked
+                    ),
+                ],
+            ),
+            self.tasks_view,
+        ]
+
+    def add_clicked(self, e):
+        self.tasks_view.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
+def main(page: ft.Page):
+    page.title = "To-Do App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    # create application instance
+    for i in range(20):
+        app =TodoApp()
+        page.add(app)
+    
+
+    # add application's root control to the page
+    #page.add(apps)
+
+if __name__ == "__main__":
+
+    qs = [
+    {
+        "pregunta": "Según el Reglamento de Instalaciones de Protección Contra Incendios, ¿cuál es el objeto principal de este Reglamento?",
+        "opciones": [
+            "La regulación de la seguridad en túneles de carreteras del Estado.",
+            "La determinación de las condiciones y requisitos exigibles al diseño, instalación, mantenimiento e inspección de los equipos y sistemas de protección activa contra incendios.",
+            "El establecimiento de las normativas para la fabricación de extintores portátiles.",
+            "La inspección de sistemas de protección pasiva contra incendios."
+        ],
+        "respuesta": "La determinación de las condiciones y requisitos exigibles al diseño, instalación, mantenimiento e inspección de los equipos y sistemas de protección activa contra incendios.",
+        "tema": "Objeto del Reglamento",
+        "dificultad": 1,
+        "creation_date": "5/11/2025",
+        "reference": "Artículo 1. Objeto y ámbito de aplicación material. 1. Constituye el objeto de este Reglamento la determinación de las condiciones y los requisitos exigibles al diseño, instalación/aplicación, mantenimiento e inspección de los equipos, sistemas y componentes que conforman las instalaciones de protección activa contra incendios."
+    },
+    {
+        "pregunta": "El Reglamento de Instalaciones de Protección Contra Incendios se aplicará con carácter supletorio, con una excepción específica. ¿Cuál es esta excepción?",
+        "opciones": [
+            "Las instalaciones de protección activa contra incendios en edificios de uso residencial vivienda.",
+            "Los túneles de carreteras del Estado, cuya regulación en materia de seguridad se regirá por el Real Decreto 635/2006.",
+            "Los sistemas de detección y alarma de incendios en zonas urbanas.",
+            "Los equipos de protección activa contra incendios sujetos al marcado CE."
+        ],
+        "respuesta": "Los túneles de carreteras del Estado, cuya regulación en materia de seguridad se regirá por el Real Decreto 635/2006.",
+        "tema": "Ámbito de aplicación supletorio",
+        "dificultad": 2,
+        "creation_date": "5/11/2025",
+        "reference": "Artículo 1.2. Asimismo, el presente Reglamento se aplicará con carácter supletorio en aquellos aspectos relacionados con las instalaciones de protección activa contra incendios no regulados en las legislaciones específicas, con la excepción de los túneles de carreteras del Estado, cuya regulación en materia de seguridad se regirá por el Real Decreto 635/2006, de 26 de mayo, sobre requisitos mínimos de seguridad en los túneles de carreteras del Estado."
+    },
+    {
+        "pregunta": "Según el Reglamento de Instalaciones de Protección Contra Incendios, ¿quiénes están sujetos a sus disposiciones?",
+        "opciones": [
+            "Solo las empresas instaladoras de sistemas de protección activa contra incendios.",
+            "Solo las empresas mantenedoras de instalaciones de protección contra incendios.",
+            "Las empresas instaladoras y mantenedoras, así como fabricantes, importadores, distribuidores u organismos que intervengan en la certificación o evaluación técnica de los productos.",
+            "Únicamente los usuarios finales de las instalaciones de protección contra incendios."
+        ],
+        "respuesta": "Las empresas instaladoras y mantenedoras, así como fabricantes, importadores, distribuidores u organismos que intervengan en la certificación o evaluación técnica de los productos.",
+        "tema": "Ámbito de aplicación subjetivo",
+        "dificultad": 2,
+        "creation_date": "5/11/2025",
+        "reference": "Artículo 2. Ámbito de aplicación subjetivo. 1. Estarán sujetos a las disposiciones de este Reglamento tanto las empresas instaladoras como las empresas mantenedoras de instalaciones de protección contra incendios. 2. Asimismo, las exigencias técnicas de este Reglamento se aplicarán a los fabricantes, importadores, distribuidores u organismos que intervengan en la certificación o evaluación técnica de los productos, y a todos aquellos que pudieran verse afectados por esta regulación."
+    },
+    {
+        "pregunta": "De acuerdo con el Artículo 3 del Reglamento de Instalaciones de Protección Contra Incendios, ¿qué se entiende por 'Protección activa contra incendios'?",
+        "opciones": [
+            "El conjunto de medidas pasivas para prevenir la propagación del fuego.",
+            "El conjunto de medios, equipos y sistemas, ya sean manuales o automáticos, cuyas funciones específicas son la detección, control y/o extinción de un incendio, facilitando la evacuación de los ocupantes e impidiendo que el incendio se propague, minimizando así las pérdidas personales y materiales.",
+            "Los productos y materiales de construcción que retardan la acción del fuego.",
+            "El sistema de evacuación de humos y calor diseñado para la seguridad de las personas."
+        ],
+        "respuesta": "El conjunto de medios, equipos y sistemas, ya sean manuales o automáticos, cuyas funciones específicas son la detección, control y/o extinción de un incendio, facilitando la evacuación de los ocupantes e impidiendo que el incendio se propague, minimizando así las pérdidas personales y materiales.",
+        "tema": "Definiciones",
+        "dificultad": 1,
+        "creation_date": "5/11/2025",
+        "reference": "Artículo 3. Definiciones. a) Protección activa contra incendios: es el conjunto de medios, equipos y sistemas, ya sean manuales o automáticos, cuyas funciones específicas son la detección, control y/o extinción de un incendio, facilitando la evacuación de los ocupantes e impidiendo que el incendio se propague, minimizando así las pérdidas personales y materiales."
+    },
+    {
+        "pregunta": "Según el Anexo I del Reglamento de Instalaciones de Protección Contra Incendios, ¿cuál es la masa máxima de un extintor portátil en condiciones de funcionamiento?",
+        "opciones": [
+            "Superior a 20 kg.",
+            "Igual o inferior a 15 kg.",
+            "Igual o inferior a 20 kg.",
+            "Cualquier masa, siempre que pueda ser transportado a mano."
+        ],
+        "respuesta": "Igual o inferior a 20 kg.",
+        "tema": "Extintores de incendio",
+        "dificultad": 1,
+        "creation_date": "5/11/2025",
+        "reference": "ANEXO I. Sección 1.ª Protección activa contra incendios. 4. Extintores de incendio. 1. En función de la carga, los extintores se clasifican de la siguiente forma: a) Extintor portátil: Diseñado para que puedan ser llevados y utilizados a mano, teniendo en condiciones de funcionamiento una masa igual o inferior a 20 kg."
+    }
+]
+    
+    ft.app(main)
